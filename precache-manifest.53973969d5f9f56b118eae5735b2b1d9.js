8e75decc84dc721d29fa3fc8242715048647b550
DimaDP <dimadp95@gmail.com>
2021-04-23 23:06:06 +0300

    Updates

---
+++ precache-manifest.53973969d5f9f56b118eae5735b2b1d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c1e1da9a7afc3104cf0477c84490637",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f4a91ddcb1a6fe13aa3c",
+    "url": "./static/css/main.d30480ef.chunk.css"
+  },
+  {
+    "revision": "a6e1a77ad71a747d9b96",
+    "url": "./static/js/2.2b728608.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.2b728608.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4a91ddcb1a6fe13aa3c",
+    "url": "./static/js/main.8fef00cc.chunk.js"
+  },
+  {
+    "revision": "4e0eef9267ed46c1c31c",
+    "url": "./static/js/runtime-main.4d21a353.js"
+  },
+  {
+    "revision": "de90c5a5eb98517d4ded97c880c75cf2",
+    "url": "./static/media/header.de90c5a5.jpg"
+  }
+]);
